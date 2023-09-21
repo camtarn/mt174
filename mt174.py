afc53cd7a2abb69f336e5192a55336d60644b958
Andy Walker <andy@sequentec.co.uk>
2023-09-21 17:11:08 -0400

    Working version
    - Fix some errors which were stopping the script from running under
      Python 3.10
    - Remove MQTT and EIBD processors
    - Remove FakeMeter class
    - Extract out baud rate into a constant, as it may need to be changed by
      library users
    - Add code to remove a couple of garbage bytes we were sometimes (but
      not always!) receiving in front of the correct meter ID
    - Fix meter ID code which wasn't working
    - Consistently use bytes throughout MT174.read(), only converting to str
      at the end
    - Run pylint and correct the most egregious warnings (which involved
      renaming some variables)
    - Add type hints
    - Add a requirements.txt
    - Add some extra documentation and logging
    - Move extract() function to MT174 class and rename it something more
      meaningful

--- mt174.py
+++ mt174.py
@@ -1,334 +1,245 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+"""
+Interface with an Iskraemeco MT174 electricity meter via a serial connection
+"""
 
-import subprocess
-import random
-import mosquitto
-import serial
+# Imports: standard library
 import time
 import re
 import logging
+from typing import Dict, Sequence
 
-
-class FakeMeter:
-
-	def __init__(self):
-		self.__index = [0, 0]
-		logging.info("Created FakeMeter")
-
-	def read(self):
-		self.__index = [x + random.random() for x in self.__index]
-		total = self.__index[0] + self.__index[1]
-		return '''0-0:F.F.0*255(0000000)
-1-0:0.0.0*255(352143)
-0-0:C.1.0*255(62791737)
-C.1.1(ISK0MT174-0001)
-1-0:1.8.1*255(%011.3f*kWh)
-1-0:1.8.2*255(%011.3f*kWh)
-1-0:1.8.0*255(%011.3f*kWh)
-1-0:2.8.1*255(0000001.012*kWh)
-1-0:2.8.2*255(0000001.612*kWh)
-1-0:2.8.0*255(0000002.624*kWh)
-1-0:32.7.0*255(233.1*V)
-1-0:52.7.0*255(232.7*V)
-1-0:72.7.0*255(233.8*V)
-1-0:31.7.0*255(5.36*A)
-1-0:51.7.0*255(8.06*A)
-1-0:71.7.0*255(5.71*A)
-1-0:36.7.0*255(1.086*kW)
-1-0:56.7.0*255(1.714*kW)
-1-0:76.7.0*255(1.220*kW)
-1-0:33.7.0*255(0.866)
-1-0:53.7.0*255(0.915)
-1-0:73.7.0*255(0.911)
-0-0:C.7.0*255(5)
-0-0:C.7.1*255(5)
-0-0:C.7.2*255(5)
-0-0:C.7.3*255(5)
-1-0:0.2.0*255(1.03)
-0-0:C.1.6*255(FDF5)''' % (self.__index[0], self.__index[1], total)
-
-
+# Imports: Pip packages
+# Package: pyserial
+import serial
 
 class MT174:
+    """
+    This class handles communications with the meter.
 
-	ACK = '\x06'
-	STX = '\x02'
-	ETX = '\x03'
-	DELAY = 0.02
+    Example meter output:
+    1-0:0.9.1*255(202511)
+    1-0:0.9.2*255(0230921)
+    1-0:0.2.0*255(1.03)
+    0-0:C.1.0*255(62807889)
+    0-0:C.1.6*255(FDF5)
+    0-0:C.51.1*255(20)
+    0-0:C.51.2*255(0230920104504)
+    0-0:C.51.2*01(0230920104504)
+    0-0:C.51.2*02(0230920094212)
+    0-0:C.51.2*03(0230920094158)
+    0-0:C.51.3*255(0)
+    0-0:C.51.4*255()
+    0-0:C.51.4*01()
+    0-0:C.51.4*02()
+    0-0:C.51.4*03()
+    0-0:C.51.5*255(0)
+    0-0:C.51.6*255()
+    0-0:C.51.6*01()
+    0-0:C.51.6*02()
+    0-0:C.51.6*03()
+    1-0:31.7.0*255(10.40*A)
+    1-0:51.7.0*255(6.40*A)
+    1-0:71.7.0*255(5.28*A)
+    1-0:31.6.0*255(1291.20*A)
+    1-0:51.6.0*255(1556.96*A)
+    1-0:71.6.0*255(1748.48*A)
+    1-0:32.7.0*255(241.0*V)
+    1-0:52.7.0*255(242.1*V)
+    1-0:72.7.0*255(241.2*V)
+    1-0:1.7.0*255(3.920*kW)
+    1-0:1.8.0*255(0692930.505*kWh)
+    1-0:2.7.0*255(0.000*kW)
+    1-0:2.8.0*255(0015803.862*kWh)
+    1-0:15.8.0*255(0708734.368*kWh)
+    1-0:5.8.0*255(0235104.302*kVArh)
+    1-0:6.8.0*255(0062222.609*kVArh)
+    1-0:7.8.0*255(0001449.672*kVArh)
+    1-0:8.8.0*255(0058260.488*kVArh)
+    1-0:9.8.0*255(0906977.544*kVAh)
+    0-0:F.F.0*255(0000000)
+    """
 
-	def __init__(self, port):
-		self.__port = port
-		logging.info("Created MT174, port = %s", port)
-	
-	@staticmethod
-	def __delay():
-		time.sleep(MT174.DELAY)
-	
-	def read(self):
-		logging.debug("Opening serial port %s", self.__port)
-		mt174 = serial.Serial(port = self.__port, baudrate=300, bytesize=7, parity='E', stopbits=1, timeout=1.5);
-		try:
-			# 1 ->
-			logging.debug("Writing hello message")
-			message = '/?!\r\n' # IEC 62056-21:2002(E) 6.3.1
-			mt174.write(message)
-			# 2 <-
-			MT174.__delay()
-			message = mt174.readline() # IEC 62056-21:2002(E) 6.3.2
-			if len(message) == 0:
-				raise Exception("Empty string instead of identification")
-			logging.debug("Got reply: %s", message)
-			if message[0] != '/':
-				raise Exception("No identification message")
-			if len(message) < 7:
-				raise Exception("Identification message to short")
-			# 3 ->
-			message = MT174.ACK + '000\r\n' # IEC 62056-21:2002(E) 6.3.3
-			mt174.write(message)
-			MT174.__delay()
-			# 4 <-
-			datablock = ""
-			if mt174.read() == MT174.STX:
-				x = mt174.read()
-				if len(x) == 0:
-					raise Exception("Empty string instead of data")
-				BCC = 0
-				while x != '!':
-					BCC = BCC ^ ord(x)
-					datablock = datablock + x
-					x = mt174.read()
-					if len(x) == 0:
-						raise Exception("Empty string instead of data")
-				while x != MT174.ETX:
-					BCC = BCC ^ ord(x) # ETX itself is part of block check
-					x = mt174.read()
-					if len(x) == 0:
-						raise Exception("Empty string instead of data")
-				BCC = BCC ^ ord(x)
-				x = mt174.read() # x is now the Block Check Character
-				# last character is read, could close connection here
-				if (BCC != ord(x)): # received correctly?
-					datablock = ""
-					raise Exception("Result not OK, try again")
-			else:
-				logging.warning("No STX found, not handled")
-			return datablock
-		finally:
-			if mt174.isOpen():
-				mt174.close()
+    ACK = b'\x06'
+    STX = b'\x02'
+    ETX = b'\x03'
+    DELAY = 0.02
+    BAUDRATE = 9600
 
-class Scheduler:
+    # e.g. 1-0:1.8.1*255(0001798.478*kWh)
+    DATABLOCK_REGEX = re.compile(r"(?:\d-\d:)?(\S+\.\S+\.\d+)(?:\*255)?\((.+)\)")
 
-	SLEEP_TIME = 0.1
+    def __init__(self, port: int):
+        self.__port = port
+        logging.info("Created MT174, port = %s", port)
 
-	def __init__(self, mt174, processors, interval = 60):
-		self.__mt174 = mt174
-		self.__processors = processors
-		self.__interval = interval
-		logging.info("Created scheduler, interval = %ds", interval)
-	
-	def execute(self, timestamp):
-		try:
-			begin = time.time()
-			data = self.__mt174.read()
-			end = time.time()
-			logging.info("Read data in %.3fs", (end - begin))
-			logging.debug("Data: %s", data)
-			for p in self.__processors:
-				try:
-					begin = time.time()
-					p.process(timestamp, data)
-					end = time.time()
-					logging.info("Processor (%s) in %.3fs", p.getName(), (end - begin))
-				except KeyboardInterrupt:
-					raise
-				except Exception:
-					logging.exception("Error in processor")
-		except KeyboardInterrupt:
-			raise
-		except Exception:
-			logging.exception("Error in meter reader")
-	
-	def run(self):
-		start = 0
-		try:
-			while True:
-				while (time.time() - start) < self.__interval:
-					time.sleep(Scheduler.SLEEP_TIME)
-				start = time.time()
-				self.execute(start)
-		except KeyboardInterrupt:
-			return 0
-		except Exception:
-			logging.exception("Error. Exiting...")
-			return 1
+    @staticmethod
+    def __delay() -> None:
+        """Sleep for a short interval to allow the meter time to process"""
+        time.sleep(MT174.DELAY)
+
+    def read(self) -> str:
+        """Perform a handshake sequence with the meter in order to read a data block"""
+        logging.debug("Opening serial port %s", self.__port)
+        mt174 = serial.Serial(port = self.__port, baudrate=MT174.BAUDRATE, bytesize=7, parity='E', stopbits=1, timeout=1.5)
+        try:
+            # 1 ->
+            logging.debug("Writing hello message")
+            message = b'/?!\r\n' # IEC 62056-21:2002(E) 6.3.1
+            mt174.write(message)
+            # 2 <-
+            logging.debug("Receiving meter identification")
+            MT174.__delay()
+            message = mt174.readline() # IEC 62056-21:2002(E) 6.3.2
+            if len(message) == 0:
+                raise Exception("Empty string instead of identification")
+            logging.debug("Got reply: %s", message)
+            # Remove possible garbage bytes. Not sure why we're getting these.
+            if message[0] != ord('/') and b'/' in message:
+                message_start_idx = message.index(b'/')
+                discarded = message[0:message_start_idx]
+                message = message[message_start_idx:]
+                logging.debug("Discarding %s garbage bytes: %s, new message is %s", message_start_idx, discarded, message)
+            if message[0] != ord('/'):
+                raise Exception("No identification message")
+            if len(message) < 7:
+                raise Exception("Identification message too short")
+            # 3 ->
+            logging.debug("Writing acknowledgement message")
+            message = MT174.ACK + b'000\r\n' # IEC 62056-21:2002(E) 6.3.3
+            mt174.write(message)
+            MT174.__delay()
+            # 4 <-
+            logging.debug("Receiving datablock")
+            datablock = b""
+            if mt174.read() == MT174.STX:
+                curr_byte = mt174.read()
+                if len(curr_byte) == 0:
+                    raise Exception("Empty string instead of data")
+                # bcc is the Block Check Character
+                bcc = 0
+                while curr_byte != b'!':
+                    bcc = bcc ^ ord(curr_byte)
+                    datablock = datablock + curr_byte
+                    curr_byte = mt174.read()
+                    if len(curr_byte) == 0:
+                        raise Exception("Empty string instead of data")
+                while curr_byte != MT174.ETX:
+                    bcc = bcc ^ ord(curr_byte) # ETX itself is part of block check
+                    curr_byte = mt174.read()
+                    if len(curr_byte) == 0:
+                        raise Exception("Empty string instead of data")
+                bcc = bcc ^ ord(curr_byte)
+                curr_byte = mt174.read() # curr_byte is now the Block Check Character
+                # last character is read, could close connection here
+                if bcc != ord(curr_byte): # received correctly?
+                    datablock = ""
+                    raise Exception("Result not OK, try again")
+            else:
+                logging.warning("No STX found, not handled")
+            return datablock.decode('us-ascii')
+        finally:
+            if mt174.isOpen():
+                mt174.close()
+
+    @staticmethod
+    def datablock_to_dict(data: str) -> Dict[ str, str ]:
+        """
+        Utility method to break the datablock string up into a dict of field
+        codes -> field values
+        """
+        data_dict = {}
+        for line in data.split():
+            match = MT174.DATABLOCK_REGEX.match(line)
+            if match:
+                data_dict[match.group(1)] = match.group(2)
+        return data_dict
+
 
 class Processor:
+    """Base class for processors used by the Scheduler"""
+    def __init__(self, name: str):
+        self.__name = name
 
-	# 1-0:1.8.1*255(0001798.478*kWh)
-	REGEX = re.compile("(?:\d\-\d:)?(\S+\.\S+\.\d+)(?:\*255)?\((.+)\)")
-	
-	def __init__(self, name):
-		self.__name = name
+    def get_name(self) -> str:
+        """Used for logging"""
+        return self.__name
 
-	def getName(self):
-		return self.__name
-
-	@staticmethod
-	def extract(data):
-		d = dict()
-		for line in data.split():
-			match = Processor.REGEX.match(line)
-			if match:
-				d[match.group(1)] = match.group(2)
-		return d
-			
-	def process(self, timestamp, data):
-		pass
-
-class MqttProcessor(Processor):
-
-	MARGIN_PERCENT = 0.5
-	
-	MOSQUITTO_PUB = "/usr/bin/mosquitto_pub"
-
-	def __init__(self, host, port, rootTopic, interval):
-		Processor.__init__(self, "mqtt")
-		self.__last = [0, (0,0,0)]
-		self.__host = host
-		self.__port = port
-		self.__rootTopic = rootTopic
-		self.__interval = interval + interval * MqttProcessor.MARGIN_PERCENT
-		logging.info("Created MQTT (%s), host = %s, port = %d", self.getName(), host, port)
-
-	def buildCommand(self, topic, value):
-		command = [MqttProcessor.MOSQUITTO_PUB]
-		command.extend(["-i", "%s" % self.getName()])
-		command.extend(["-h", "%s" % self.__host])
-		command.extend(["-p", "%d" % self.__port])
-		command.extend(["-t", "%s/%s" % (self.__rootTopic, topic)])
-		command.extend(["-m", "%s" % value])
-		return command
-
-	def executeCommand(self, command):
-		with open("/dev/null", "w") as f:
-			logging.debug("Executing: %s" % " ".join(command))
-			retcode = subprocess.call(command, stdout = f, stderr = f)
-			if retcode != 0:
-				logging.error("Unable to execute command")
-			return retcode
-
-	def process(self, timestamp, data):
-		d = Processor.extract(data)
-		edis = ("1.8.0", "1.8.1", "1.8.2")
-		names = ("total", "tariff1", "tariff2")
-		index = [float(d[x].split("*")[0]) for x in d if x in edis]
-		current = [timestamp, index]
-		# current
-		if self.__last[0] != 0:
-			interval = current[0] - self.__last[0]
-			if interval < self.__interval:
-				func = lambda x,y: (x - y) * 3600. / interval
-				values = map(func, current[1], self.__last[1])
-				for key, value in zip(names, values):
-					message = "%d %.3f" % (timestamp, value)
-					topic = "current/%s" % key
-					command = self.buildCommand(topic, message)
-					self.executeCommand(command)
-			else:
-				logging.error("Interval too long: %.2f" % interval)
-		# index
-		for key, value in zip(names, index):
-			message = "%d %.3f" % (timestamp, value)
-			topic = "index/%s" % key
-			command = self.buildCommand(topic, message)
-			self.executeCommand(command)
-			logging.info("%s = %.3f kWh", topic, value)
-		# power down counter
-		message = "%d %d" % (timestamp, int(d["C.7.0"]))
-		topic = "powerdown/counter"
-		command = self.buildCommand(topic, message)
-		self.executeCommand(command)
-
-		self.__last = current
-
-class EibdProcessor(Processor):
-	
-	GROUPWRITE = "/usr/local/bin/groupwrite"
-
-	def __init__(self, host):
-		Processor.__init__(self, "eibd")
-		self.__host = host
-		logging.info("Created EIBD (%s), host = %s", self.getName(), host)
-	
-	@staticmethod
-	def convertTo4Bytes(value):
-		if value < 0:
-			raise Exception("Value smaller than 0: %d", value)
-		elif value > 2147483647:
-			raise Exception("Value greater than 2147483647: %d", value)
-		else:
-			number = "{0:0{1}x}".format(value, 8)
-			return ["0x" + number[i:i+2] for i in xrange(0, len(number), 2)]
-
-	def buildCommand(self, groupAddress, bytes):
-		command = [EibdProcessor.GROUPWRITE]
-		command.append("%s" % self.__host)
-		command.append("%s" % groupAddress)
-		command.extend(bytes)
-		return command
-
-	def executeCommand(self, command):
-		with open("/dev/null", "w") as f:
-			logging.debug("Executing: %s" % " ".join(command))
-			#retcode = subprocess.call(command, stdout = f, stderr = f)
-			retcode = 0
-			if retcode != 0:
-				logging.error("Unable to execute command")
-			return retcode
-
-	def process(self, timestamp, data):
-		d = Processor.extract(data)
-		edis = ("1.8.0", "1.8.1", "1.8.2")
-		ga = ("14/1/0", "14/1/1", "14/1/2")
-		index = [float(d[x].split("*")[0]) for x in d if x in edis]
-		# index
-		for key, value in zip(ga, index):
-			Wh = int(value * 1000)
-			command = self.buildCommand(key, EibdProcessor.convertTo4Bytes(Wh))
-			self.executeCommand(command)
-			logging.info("%s = %d Wh", key, Wh)
-		# power down counter
-		key = "14/1/3"
-		value = int(d["C.7.0"])
-		command = self.buildCommand(key, [hex(value)])
-		logging.info("%s = %d times", key, value)
-		self.executeCommand(command)
-
+    def process(self, timestamp: float, data: str) -> None:
+        """Main entry point for subclasses, called from Scheduler"""
 
 class FileLogger(Processor):
+    """A simple logger which writes to a file in /tmp"""
+    def __init__(self, filename):
+        Processor.__init__(self, "file-logger")
+        self.__filename = filename
+        logging.info("Created FileLogger, filename = %s", filename)
 
-	def __init__(self, filename):
-		Processor.__init__(self, "file-logger")
-		self.__filename = filename
-		logging.info("Created FileLogger, filename = %s", filename)
+    def process(self, timestamp: float, data: str) -> None:
+        """Main entry point for subclasses, called from Scheduler"""
+        filename = "%s-%s.log" % (self.__filename, time.strftime("%Y-%m"))
+        with open(filename, "a+", encoding="us-ascii") as output:
+            output.write("%d: %s\n" % (timestamp, MT174.datablock_to_dict(data)))
+            logging.debug("Written data to %s", filename)
 
-	def process(self, timestamp, data):
-		filename = "%s-%s.log" % (self.__filename, time.strftime("%Y-%m"))
-		with open(filename, "a+") as f:
-			f.write("%d: %s\n" % (timestamp, Processor.extract(data)))
-			logging.debug("Written data to %s", filename)
+class Scheduler:
+    """
+    A simple scheduler which runs the MT174 meter reading code at a
+    configurable interval, then feeds the resulting data block to a list of
+    processors (e.g. a local logger and an MQTT client)
+    """
+    SLEEP_TIME = 0.1
+
+    def __init__(self, mt174: MT174, processors: Sequence[ Processor ], interval: float = 60):
+        self.mt174 = mt174
+        self.processors = processors
+        self.interval = interval
+        logging.info("Created scheduler, interval = %ds", interval)
+
+    def execute(self, timestamp: float) -> None:
+        try:
+            begin = time.time()
+            data = self.mt174.read()
+            end = time.time()
+            logging.info("Read data in %.3fs", (end - begin))
+            logging.debug("Data: %s", data)
+            for processor in self.processors:
+                try:
+                    begin = time.time()
+                    processor.process(timestamp, data)
+                    end = time.time()
+                    logging.info("Processor (%s) in %.3fs", processor.get_name(), (end - begin))
+                except KeyboardInterrupt:
+                    raise
+                except Exception:
+                    logging.exception("Error in processor")
+        except KeyboardInterrupt:
+            raise
+        except Exception:
+            logging.exception("Error in meter reader")
+
+    def run(self) -> None:
+        start = 0
+        try:
+            while True:
+                while (time.time() - start) < self.interval:
+                    time.sleep(Scheduler.SLEEP_TIME)
+                start = time.time()
+                self.execute(start)
+        except KeyboardInterrupt:
+            return 0
+        except Exception:
+            logging.exception("Error. Exiting...")
+            return 1
 
 
-logging.basicConfig(format = "%(levelname)s: %(message)s")
-logging.getLogger().setLevel(logging.DEBUG)
 
-interval = 60
-m = MT174("/dev/ttyUSB0")
-#m = FakeMeter()
-#p = (Logger(),)
-#p = (MqttProcessor("localhost", 1883, "/home/energy", interval), FileLogger("/tmp/data"))
-p = (EibdProcessor("local:/tmp/eib"), FileLogger("/tmp/data"))
-s = Scheduler(m, p, interval)
-s.run()
+if __name__ == "__main__":
+    logging.basicConfig(format = "%(levelname)s: %(message)s")
+    logging.getLogger().setLevel(logging.DEBUG)
 
+    INTERVAL = 60
+    mt174 = MT174("/dev/ttyUSB0")
+    processors = [FileLogger("/tmp/data")]
+    scheduler = Scheduler(mt174, processors, INTERVAL)
+    scheduler.run()
